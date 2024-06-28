8e7da98c4afc12b67dc32c630a208dee2deb8edf
RoWeS <129057401+rowes-star@users.noreply.github.com>
2024-06-28 14:42:27 +0300

    Update id.py
    Signed-off-by: RoWeS <129057401+rowes-star@users.noreply.github.com>

--- AnonXMusic/plugins/tools/id.py
+++ AnonXMusic/plugins/tools/id.py
@@ -10,27 +10,27 @@ async def iddlock(client: Client, message):
    get = await client.get_chat_member(message.chat.id, message.from_user.id)  
    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
       if message.chat.id in iddof:
-        return await message.reply_text("**الامر معطل من قبل .**")
+        return await message.reply_text("<b>الامر معطل من قبل .</b>")
       iddof.append(message.chat.id)
-      return await message.reply_text("**تم تعطيل الايدي بنجاح .**")
+      return await message.reply_text("<b>تم تعطيل الايدي بنجاح .</b>")
    else:
-      return await message.reply_text("**عذرا عزيزي هذا الامر للادمن الجروب فقط .**")
+      return await message.reply_text("<b>عذرا عزيزي هذا الامر للادمن الجروب فقط .</b>")
 
 @app.on_message(filters.command(["فتح الايدي", "تفعيل الايدي"], "")& filters.group)
 async def iddopen(client: Client, message):
    get = await client.get_chat_member(message.chat.id, message.from_user.id)
    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
       if not message.chat.id in iddof:
-        return await message.reply_text("**الايدي مفعل من قبل .**")
+        return await message.reply_text("<b>الايدي مفعل من قبل .</b>")
       iddof.remove(message.chat.id)
-      return await message.reply_text("**تم تفعيل الايدي بنجاح .**")
+      return await message.reply_text("<b>تم تفعيل الايدي بنجاح .</b>")
    else:
-      return await message.reply_text("**عذرا عزيزي هذا الامر للادمن الجروب فقط .**")
+      return await message.reply_text("<b>عذرا عزيزي هذا الامر للادمن الجروب فقط .</b>")
 
 @app.on_message(filters.command(["ايدي"], ""))
 async def muid(client: Client, message):
        if message.chat.id in iddof:
-         return await message.reply_text("**تم تعطيل امر الايدي من قبل المشرفين .**")
+         return await message.reply_text("<b>تم تعطيل امر الايدي من قبل المشرفين .</b>")
        user = await client.get_chat(message.from_user.id)
        user_id = user.id
        username = user.username
@@ -41,7 +41,7 @@ async def muid(client: Client, message):
        if not id.get(message.from_user.id):
          id[user.id] = []
        idd = len(id[user.id])
-       await message.reply_photo(photo=photo, caption=f"**name : {first_name}\nid : {user_id}\nuser : [@{username}]\nbio : {bioo}**",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{idd} 💕", callback_data=f"heart{user_id}")],]),)
+       await message.reply_photo(photo=photo, caption=f"<b>name : {first_name}\nid : {user_id}\nuser : [@{username}]\nbio : {bioo}</b>",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{idd} 💕", callback_data=f"heart{user_id}")],]),)
             
 
 
